2aba8414f2ea5aa8b8e0955f7f41e5799814ac60
yellows8 <yellows8@users.noreply.github.com>
2014-12-24 03:02:37 -0500

    Initial commit.

---
+++ generate_themelz.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+//Build with: gcc -o generate_themelz lz11.c generate_themelz.c
+
+//This code simulates how Home Menu handles theme data decompression.
+
+int decompress_lz11(unsigned char *compressed_datain, unsigned char *decompressed_dataout, int insize, int maxoutsize);
+
+int main(int argc, char **argv)
+{
+	FILE *f;
+	unsigned char *buffer;
+	int insize = 0;
+	int ret;
+	struct stat filestats;
+
+	if(argc<3)return 0;
+
+	if(stat(argv[1], &filestats)==-1)
+	{
+		printf("Failed to stat input.\n");
+		return 1;
+	}
+
+	insize = filestats.st_size;
+	if(insize > 0x150000)
+	{
+		printf("Input file is too large.\n");
+		return 3;
+	}
+
+	f = fopen(argv[1], "rb");
+	if(f==NULL)
+	{
+		printf("Failed to open input file for reading.\n");
+		return 2;
+	}
+
+	buffer = (unsigned char*)malloc(0x400000);
+	if(buffer==NULL)
+	{
+		printf("Failed to alloc mem.\n");
+		fclose(f);
+		return 4;
+	}
+	memset(buffer, 0, 0x400000);
+
+	fread(&buffer[0x150000], 1, insize, f);
+
+	fclose(f);
+
+	ret = decompress_lz11(&buffer[0x150000], buffer, insize, 0x400000);
+	printf("decompress_lz11() retval: %d\n", ret);
+
+	f = fopen(argv[2], "wb");
+	if(f==NULL)
+	{
+		printf("Failed to open output file for writing.\n");
+		free(buffer);
+		return 2;
+	}
+
+	fwrite(buffer, 1, 0x400000, f);
+
+	fclose(f);
+
+	return 0;
+}
+
---
+++ lz11.c
@@ -0,0 +1,109 @@
+#include <string.h>
+#include <stdio.h>
+
+//Note that the actual Home Menu decompression function doesn't have an outsize parameter.
+int lz11Decompress(unsigned char *src, unsigned char *dst, int insize, int outsize) { //Based on the code from here, modified to mostly match the Home Menu code: https://github.com/mtheall/decompress/blob/master/source/lz11.c
+  unsigned int i;
+  unsigned char flags;
+  int pos=0, pos2;
+  unsigned char *original_dstval = dst;
+  unsigned char *original_dst = dst;
+
+  while(outsize > 0) {
+    // read in the flags data
+    // from bit 7 to bit 0, following blocks:
+    //     0: raw byte
+    //     1: compressed block
+    if(insize==0)return -2;
+    flags = *src++;
+    insize--;
+    for(i = 0; i < 8 && outsize > 0; i++, flags <<= 1) {
+      if(flags&0x80) { // compressed block
+        int len;  // length
+        int disp; // displacement
+        switch((*src)>>4) {
+          case 0: // extended block
+            if(insize==0)return -2;
+            len   = (*src++)<<4;
+            len  |= ((*src)>>4);
+            len  += 0x11;
+            insize--;
+            break;
+          case 1: // extra extended block
+            if(insize<=1)return -2;
+            len   = ((*src++)&0x0F)<<12;
+            len  |= (*src++)<<4;
+            len  |= ((*src)>>4);
+            len  += 0x111;
+            insize-=2;
+            break;
+          default: // normal block
+            len   = ((*src)>>4)+1;
+        }
+
+        if(insize<=1)return -2;
+        disp  = ((*src++)&0x0F)<<8;
+        disp |= *src++;
+        disp++;
+        insize-=2;
+
+        if(len > outsize || pos-disp < 0)return -4;
+
+        outsize -= len;
+
+        // for len, copy data from the displacement
+        // to the current buffer position
+        for(pos2=0; pos2<len; pos2++)original_dst[pos2+pos] = original_dst[pos2+pos-disp];
+        dst += len;
+        pos += len;
+      }
+
+      else { // uncompressed block
+        // copy a raw byte from the input to the output
+        *dst++ = *src++;
+        pos++;
+        insize--;
+        outsize--;
+      }
+
+      if(outsize==0)
+      {
+          if(insize >= 32)return -3;
+          return 0;
+      }
+    }
+  }
+
+  return 0;
+}
+
+int decompress_lz11(unsigned char *compressed_datain, unsigned char *decompressed_dataout, int insize, int maxoutsize)
+{
+	int decom_size = 0;
+
+	if(compressed_datain[0] != 0x11)return -1;
+
+	decom_size = compressed_datain[1] | (compressed_datain[2]<<8) | (compressed_datain[3]<<16);
+
+	if(decom_size==0)
+	{
+		if(insize < 4)return -2;
+		insize-=4;
+		compressed_datain+=4;
+
+		decom_size = compressed_datain[1] | (compressed_datain[2]<<8) | (compressed_datain[3]<<16);
+		if(decom_size==0)
+		{
+			if(insize >= 32)return -3;
+			return 0;
+		}
+	}
+
+	if(decom_size > maxoutsize)return -9;//Not a Home Menu error.
+
+	insize-=4;
+	compressed_datain+=4;
+
+	return lz11Decompress(compressed_datain, decompressed_dataout, insize, decom_size);
+}
+
